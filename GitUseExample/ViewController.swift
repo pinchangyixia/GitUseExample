2afd9892e18c40f12d39b4ab5188e88a1daa458b
Besttone <besttone@BesttonedeMacBook-Pro.local>
2018-05-14 14:47:27 +0800

    Initial Commit

---
+++ GitUseExample/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GitUseExample
+//
+//  Created by Besttone on 2018/5/14.
+//  Copyright © 2018年 Besttone. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
